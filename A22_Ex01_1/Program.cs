604f141cdcdeec793e88209ad4cba561d469528c
Nati Parnes <parnes.nati@gmail.com>
2021-11-11 17:28:23 +0200

    execrise 1 progress

--- A22_Ex01_1/Program.cs
+++ A22_Ex01_1/Program.cs
@@ -10,11 +10,38 @@ namespace A22_Ex01_1
     {
         public static void Main()
         {
-
+            Ex01_1();
         }
-        public int BinaryToDecimal(string BinNum)
+        public static void Ex01_1()
         {
-
+            string binary1,binary2,binary3,binary4;
+            bool validationFlag = true;
+            System.Console.WriteLine("Hello, please enter 4 binary numbers, 8 bit sized each");
+            binary1 = System.Console.ReadLine();
+            binary2 = System.Console.ReadLine();
+            binary3 = System.Console.ReadLine();
+            binary4 = System.Console.ReadLine();
+            while (validationFlag)
+            {
+                
+            }
+            System.Console.WriteLine(temp);
+        }
+        public static int BinaryToDecimal(string BinNum)
+        {
+            int NumberInDecimal =0;
+            for(int i = 0; i<BinNum.Length; i++)
+            {
+                if (BinNum[i] == '1')
+                NumberInDecimal += (int)Math.Pow(2, BinNum.Length-i-1);
+            }
+            return NumberInDecimal;
+        }
+        private static bool isInputValid(string i_binNumber)
+        {
+            if (i_binNumber.Length != 8) return false;
+            if ((!(i_binNumber.Contains("1")))&& (!(i_binNumber.Contains("0")))) return false;
+            return true;
         }
     }
 }
