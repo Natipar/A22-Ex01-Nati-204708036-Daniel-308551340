c783e71382c0faee38d06ae84115ffd89ff31fe6
parne <parne@DESKTOP-RFKU590>
2021-11-11 15:55:43 +0200

    Add project files.

---
+++ A22_Ex01_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A22_Ex01_1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+        }
+
+    }
+}
